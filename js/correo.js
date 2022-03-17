42c76e2889b02a8eb46487f9f103942682dba357
EdwinTorrado15 <edwintorrado15@gmail.com>
2022-03-17 11:19:09 -0500

    Modificaciones finales del proyecto

--- js/correo.js
+++ js/correo.js
@@ -15,6 +15,7 @@ async function handleSubmit(event){
     })
     if(response.ok){
         this.reset()
+        // Alertas modal
         Swal.fire({
             title: 'Gracias!',
 	        text: 'Me estare contactando contigo muy pronto!',
@@ -27,48 +28,3 @@ async function handleSubmit(event){
         })
     }
 }
-
-//Validacion del correo electronico
-// const inputs = document.querySelectorAll('#form input'); 
-// const expresiones = {
-
-// 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-//     telefono: /^[+]\d{2,3}\s\d{7,14}$/, // +Numeros 7 a 14 numeros.
-// 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-//     proyecto: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-// 	mensaje: /^[a-zA-ZÀ-ÿ\s]{1,40}$/ // Letras y espacios, pueden llevar acentos.
-// }
-
-// const validarFormulario = (e) => {
-//    switch (e.target.name) {
-//        case "nombre":
-//             if(expresiones.nombre.test(e.target.value)){
-//                 document.getElementById('grupo-nombre').classList.remove('invalid-feedback');
-//                 document.getElementById('grupo-nombre').classList.add('valid-feedback');
-//             }else{
-//                 document.getElementById('grupo-nombre').classList.add('invalid-feedback');
-//             }
-//        break;
-//        case "telefono":
-       
-//        break;
-//        case "email":
-       
-//        break;
-//        case "proyecto":
-       
-//        break;
-//        case "mensaje":
-       
-//        break;
-//    }
-// }
-
-// inputs.forEach((input) => {
-//     input.addEventListener('keyup' , validarFormulario);
-//     input.addEventListener('blur' , validarFormulario);
-// });
-
-// $form.addEventListener('submit', (e) => {
-//     e.preventDefault
-// })
